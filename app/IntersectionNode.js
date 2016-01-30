380579be5c233c8bfa80bad5d67fc9ecbb8f3bca
Justin Giannone <jman.giannone@gmail.com>
2016-01-30 03:32:43 -0500

    Fix properties

--- app/IntersectionNode.js
+++ app/IntersectionNode.js
@@ -12,21 +12,21 @@ class IntersectionNode {
 		this._containsCar = false;
 	}
 	
-	get x { return this._x; }
-	get y { return this._y; }
+	get x() { return this._x; }
+	get y() { return this._y; }
 	
-	get north { return this._north; }
-	set north { this._north = value; }
+	get north() { return this._north; }
+	set north() { this._north = value; }
 	
-	get south { return this._south; }
-	set south { this._south = value; }
+	get south() { return this._south; }
+	set south() { this._south = value; }
 	
-	get east { return this._east; }
-	set east { this._east = value; }
+	get east() { return this._east; }
+	set east() { this._east = value; }
 	
-	get west { return this._west; }
-	set west { this._west = value; }
+	get west() { return this._west; }
+	set west() { this._west = value; }
 	
-	get containsAccident { return this._containsAccident; }
-	get containsCar { return this._containsCar; }
+	get containsAccident() { return this._containsAccident; }
+	get containsCar() { return this._containsCar; }
 }
